30877e437835b853a89b3db08584f3df5d26dc35
Emilia-Tarascio-udesa <etarascio@udesa.edu.ar>
2023-05-31 16:09:21 -0300

    Register.js y FormRegister.js

--- src/components/FormRegister.js
+++ src/components/FormRegister.js
@@ -1,75 +1,157 @@
-import { Text, View, TextInput, TouchableOpacity, StyleSheet} from 'react-native'
+import { Text, View, TextInput, TouchableOpacity, StyleSheet } from 'react-native'
 import React, { Component } from 'react'
-import {auth, db} from '../firebase/config'
+import { auth, db } from '../firebase/config'
 
 export default class FormRegister extends Component {
-    constructor(props){
+    constructor(props) {
         super(props)
         this.state = {
+            inputNombre: '',
             inputMail: '',
-            inputPassword:''
+            inputDireccion: '',
+            inputTelefono: '',
+            inputPassword: '',
         }
     }
 
-    registrarUsuario(mail, password){
+    registrarUsuario(mail, password) {
         auth.createUserWithEmailAndPassword(mail, password)
-        .then( data => {
-            this.props.navigation.navigate('HomeNav')
-            db.collection('users').add({
-                owner:auth.currentUser.email,
-                createdAt: Date.now()
+            .then(data => {
+                this.props.navigation.navigate('HomeNav')
+                db.collection('users').add({
+                    owner: auth.currentUser.email,
+                    createdAt: Date.now()
+                })
+                    .then(resp => console.log(resp))
+                    .catch(err => console.log(err))
             })
-            .then(resp => console.log(resp))
             .catch(err => console.log(err))
-        })
-        .catch(err => console.log(err))
     }
-    
+
+    validarCampo(texto) {
+        return text.length >= 3
+    }
+
+    validarTelefono(numero) {
+        let numstring = numero.toString()
+        return numstring.length === 10
+    }
+
     render() {
         return (
-          <View>
-            <TextInput
-                style={styles.input}
-                placeholder='Digita su correo electronico'
-                keyboardType='email-address'
-                onChangeText={(text)=> this.setState({inputMail: text}) }
-                value={this.state.inputMail}
-            />
-            <TextInput
-                style={styles.input}
-                placeholder='Digita tu password'
-                onChangeText={(text) => this.setState({inputPassword: text})}
-                value={this.state.inputPassword}
-                secureTextEntry={true}
-            />
-            <TouchableOpacity
-                style={styles.btn}
-                onPress={()=> this.registrarUsuario(this.state.inputMail, this.state.inputPassword)}
-            >
-                <Text style={styles.btnText}>Registrar mi usuario</Text>
-            </TouchableOpacity>
-          </View>
+            <View style={styles.contenedor}>
+                <Text style={styles.title}>Registrar</Text>
+                <TextInput
+                    style={styles.input}
+                    placeholder='Nombre'
+                    keyboardType='default'
+                    onChange={(text) => {
+                        if (this.validarCampo(texto)) {
+                            this.setState({ inputNombre: text })
+                        } else {
+                            console.log('Longitud de nombre no válida.');
+                        }
+                    }}
+                    value={this.state.inputNombre}
+                />
+                <TextInput
+                    style={styles.input}
+                    placeholder='Email'
+                    keyboardType='email-address'
+                    onChangeText={(text) => {
+                        if (this.validarCampo(text)) {
+                            this.setState({ inputMail: text })
+                        } else {
+                            console.log('Longitud de email no válida.');
+                        }
+                    }}
+                    value={this.state.inputMail}
+                />
+                <TextInput
+                    style={styles.input}
+                    placeholder='Dirección'
+                    keyboardType='default'
+                    onChange={(text) => {
+                        if (this.validarCampo(texto)) {
+                            this.setState({ inputDireccion: text })
+                        } else {
+                            console.log('Longitud de dirección no válida.');
+                        }
+                    }}
+                    value={this.state.inputDireccion}
+                />
+                <TextInput
+                    style={styles.input}
+                    placeholder='Teléfono'
+                    keyboardType='numeric'
+                    onChange={(text) => {
+                        if (this.validarTelefono(texto)) {
+                            this.setState({ inputTelefono: text })
+                        } else {
+                            console.log('Longitud de teléfono no válida.');
+                        }
+                    }}
+                    value={this.state.inputTelefono}
+                />
+                <TextInput
+                    style={styles.input}
+                    placeholder='Password'
+                    onChangeText={(text) => {
+                        if (this.validarCampo(text)) {
+                            this.setState({ inputPassword: text })
+                        } else {
+                            console.log('Longitud de contraseña no válida.');
+                        }
+                    }}
+                    value={this.state.inputPassword}
+                    secureTextEntry={true}
+                />
+                <TouchableOpacity
+                    style={styles.btn}
+                    onPress={() => this.registrarUsuario(this.state.inputMail, this.state.inputPassword)}
+                >
+                    <Text style={styles.btnText}>Registrar mi usuario</Text>
+                </TouchableOpacity>
+            </View>
         )
-      }
+    }
 }
 
 const styles = StyleSheet.create({
-    input:{
-        borderWidth:1,
-        borderColor: '#3d3d3d',
+    contenedor: {
+        width: '40%',
+        marginTop: 32,
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: 'white',
+        borderRadius: 10,
+    },
+    input: {
+        borderWidth: 1,
+        borderColor: '#151515',
+        borderRadius: 5,
+        width: '90%',
         marginTop: 24,
-        height:24,
-        padding:5
+        height: 24,
+        padding: 5
     },
-    btn:{
-        marginTop:32,
-        backgroundColor: '#54d0e0',
+    btn: {
+        marginTop: 32,
+        backgroundColor: '#74549B',
         padding: 10,
-        borderRadius:20,
+        borderRadius: 20,
+        margin: 5,
     },
-    btnText:{
-        textAlign:'center',
-        fontWeight:'bold',
-        color:'white'
-    }
+    btnText: {
+        textAlign: 'center',
+        fontWeight: 'bold',
+        color: 'white'
+    },
+    title: {
+        textAlign: 'center',
+        fontWeight: 'bold',
+        color: 'black',
+        fontSize: 24,
+    },
 })
--- src/screens/Register.js
+++ src/screens/Register.js
@@ -1,4 +1,4 @@
-import { Text, View, TouchableOpacity} from 'react-native'
+import { Text, View, TouchableOpacity, StyleSheet} from 'react-native'
 import React, { Component } from 'react'
 import FormRegister from '../components/FormRegister'
 import { auth } from '../firebase/config'
@@ -15,14 +15,14 @@ class Register extends Component {
 
   render() {
     return (
-      <View>
+      <View style={styles.contenedor}>
         <FormRegister navigation={this.props.navigation}/>
-        <Text>
-            Ya tienes cuenta? 
+        <Text style={styles.btnText}>
+            ¿Ya tienes cuenta? 
             <TouchableOpacity
                 onPress={()=> this.props.navigation.navigate('Login')}
             >
-                <Text>Ingresa aquí</Text>
+                <Text style={styles.btnText}> Ingresa aquí</Text>
             </TouchableOpacity>
         </Text>
       </View>
@@ -31,3 +31,16 @@ class Register extends Component {
 }
 
 export default Register
+
+const styles = StyleSheet.create({
+  contenedor: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#74549B'
+  },
+  btnText: {
+    textAlign: 'center',
+    color: '#fff',
+  }
+})
