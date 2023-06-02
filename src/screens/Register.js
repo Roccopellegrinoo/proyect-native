fb6c9795afe8193d82808508d8726adb0f1153ca
Emilia-Tarascio-udesa <etarascio@udesa.edu.ar>
2023-06-02 14:01:21 -0300

    hago cambios en el register

--- src/screens/Register.js
+++ src/screens/Register.js
@@ -1,29 +1,20 @@
-import { Text, View, TouchableOpacity, StyleSheet} from 'react-native'
+import { Text, View, TouchableOpacity, StyleSheet } from 'react-native'
 import React, { Component } from 'react'
 import FormRegister from '../components/FormRegister'
-import { auth } from '../firebase/config'
 
 class Register extends Component {
-
-    componentDidMount(){
-        auth.onAuthStateChanged( user => {
-            if(user){
-                this.props.navigation.navigate('HomeNav')
-            }
-        })
-    }
 
   render() {
     return (
       <View style={styles.contenedor}>
-        <FormRegister navigation={this.props.navigation}/>
-        <Text style={styles.btnText}>
-            ¿Ya tienes cuenta? 
-            <TouchableOpacity
-                onPress={()=> this.props.navigation.navigate('Login')}
-            >
-                <Text style={styles.btnText}> Ingresa aquí</Text>
-            </TouchableOpacity>
+        <FormRegister navigation={this.props.navigation} />
+        <Text style={styles.text}>
+          ¿Ya tienes cuenta?<Text> </Text>
+          <TouchableOpacity
+            onPress={() => this.props.navigation.navigate('Login')}
+          >
+            <Text style={styles.btnText}>Ingresa aquí</Text>
+          </TouchableOpacity>
         </Text>
       </View>
     )
@@ -37,10 +28,17 @@ const styles = StyleSheet.create({
     display: 'flex',
     alignItems: 'center',
     justifyContent: 'center',
-    backgroundColor: '#74549B'
+    backgroundColor: '#74549B',
+    height: '100vh'
+  },
+  text: {
+    textAlign: 'center',
+    color: '#fff',
   },
   btnText: {
     textAlign: 'center',
     color: '#fff',
+    fontWeight: 'bold',
+    textDecorationLine: 'underline'
   }
 })
