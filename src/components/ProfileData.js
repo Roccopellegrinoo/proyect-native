646f1d28f6feb13b9f481c618a482619575bbf71
Emilia-Tarascio-udesa <etarascio@udesa.edu.ar>
2023-06-14 22:13:08 -0300

    agrego estilos

--- src/components/ProfileData.js
+++ src/components/ProfileData.js
@@ -4,51 +4,112 @@ import { auth } from '../firebase/config'
 
 export default class ProfileData extends Component {
 
-    logout(){
-        auth
-        .signOut()
-        .then(resp => this.props.navigation.navigate('Login'))
-        .catch(err => console.log(err))
-    }
-    render() {
-      return (
-        <View style={styles.contenedor}>
-          <View style={{ display: 'flex', flexDirection: 'row' }}> {/* Contenedor datos perfil */}
-            <View style={{ borderRadius: '50%' }}> {/* Contenedor datos foto perfil */}
-              <Image
-                source={{ uri: 'https://picsum.photos/200/300' }}
-                style={{ width: 150, height: 150, borderRadius: 100 }}
-              />
-            </View>
-            <View style={{ display: 'flex', flexDirection: 'column', marginLeft: 50, justifyContent: 'center' }}> {/* Contenedor información del perfil */}
-              <View style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', paddingBottom: 10 }}>
-                <Text style={{ fontSize: 18, fontWeight: 'bold', marginRight: 15, textTransform: 'capitalize' }}>{auth.currentUser.email}</Text>
-                <TouchableOpacity onPress={() => this.logout()} style={styles.btn} ><Text style={styles.btnText}>Cerrar sesión</Text></TouchableOpacity>
-              </View>
-              <Text style={{ fontSize: 16, paddingBottom: 5 }}>{this.props.lenPosteos} posts</Text> {/* cantidad de posteos */}
-              <Text style={{ fontSize: 16 }}>Esta es la mini descripción del usuario.</Text> {/* mini descripción del usuario */}
-            </View>
+  logout() {
+    auth
+      .signOut()
+      .then(resp => this.props.navigation.navigate('Login'))
+      .catch(err => console.log(err));
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.profileContainer}>
+          <Image
+            source={{ uri: 'https://picsum.photos/200/300' }}
+            style={styles.profilePicture}
+          />
+          <View style={styles.profileInfoContainer}>
+            <Text style={styles.emailText}>{auth.currentUser.email}</Text>
+            <TouchableOpacity onPress={() => this.logout()} style={styles.logoutButton}>
+              <Text style={styles.logoutButtonText}>Cerrar sesión</Text>
+            </TouchableOpacity>
           </View>
         </View>
-    )
+        <Text style={styles.descriptionText}>Esta es la mini descripción del usuario.</Text>
+        <View style={styles.statsContainer}>
+          <View style={styles.statItem}>
+            <Text style={styles.statNumber}>{this.props.lenPosteos}0</Text>
+            <Text style={styles.statLabel}>Publicaciones</Text>
+          </View>
+          <View style={styles.statItem}>
+            <Text style={styles.statNumber}>256</Text>
+            <Text style={styles.statLabel}>Seguidores</Text>
+          </View>
+          <View style={styles.statItem}>
+            <Text style={styles.statNumber}>128</Text>
+            <Text style={styles.statLabel}>Siguiendo</Text>
+          </View>
+        </View>
+      </View>
+    );
   }
 }
 
 const styles = StyleSheet.create({
-  btn: {
-    // marginTop: 32,
-    backgroundColor: '#74549B',
-    padding: 5,
-    borderRadius: 20,
-    // margin: 5,
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#F5F5F5',
+    paddingTop: 100,
+    paddingBottom: 40,
   },
-  btnText: {
-    fontSize: 14,
-    textAlign: 'center',
+  profileContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 20,
+  },
+  profilePicture: {
+    width: 120,
+    height: 120,
+    borderRadius: 60,
+    marginRight: 20,
+  },
+  profileInfoContainer: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  emailText: {
+    fontSize: 18,
     fontWeight: 'bold',
-    color: 'white'
+    marginBottom: 10,
+    color: '#333333',
   },
-  contenedor: {
-    margin: 10,
-  }
-})
+  logoutButton: {
+    backgroundColor: '#BC74B8',
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 20,
+  },
+  logoutButtonText: {
+    fontSize: 14,
+    fontWeight: 'bold',
+    color: '#FFFFFF',
+    textAlign: 'center',
+  },
+  descriptionText: {
+    fontSize: 16,
+    marginBottom: 20,
+    color: '#555555',
+  },
+  statsContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    width: '100%',
+    marginBottom: 20,
+  },
+  statItem: {
+    alignItems: 'center',
+  },
+  statNumber: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: '#BC74B8',
+  },
+  statLabel: {
+    fontSize: 12,
+    color: '#555555',
+    marginTop: 5,
+  },
+});
--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -1,65 +1,87 @@
-import { View, Text, FlatList, Image, StyleSheet  } from 'react-native'
-import React, {Component} from 'react'
-import ProfileData from '../components/ProfileData'
-import Header from '../components/Header'
-import {db, auth} from '../firebase/config'
+import React, { Component } from 'react';
+import { View, Text, FlatList, Image, StyleSheet } from 'react-native';
+import ProfileData from '../components/ProfileData';
+import Header from '../components/Header';
+import { db, auth } from '../firebase/config';
 
 class Profile extends Component {
-  constructor(props){
-    super(props)
+  constructor(props) {
+    super(props);
     this.state = {
       usuarios: [],
-      loading: true
-    }
+      loading: true,
+    };
   }
 
-  componentDidMount(){
-    db.collection('users').onSnapshot(
-      docs => {
-        let arrUsuarios = []
+  componentDidMount() {
+    db.collection('users').onSnapshot(docs => {
+      let arrUsuarios = [];
 
-        docs.forEach(doc => arrUsuarios.push({
+      docs.forEach(doc =>
+        arrUsuarios.push({
           id: doc.id,
-          data: doc.data()
-        }))
-
-        this.setState({
-          usuarios: arrUsuarios,
-          loading:false
+          data: doc.data(),
         })
-      }
-    )
+      );
+
+      this.setState({
+        usuarios: arrUsuarios,
+        loading: false,
+      });
+    });
   }
 
-  render(){
+  render() {
     return (
-      <View>
-         <View style={styles.header}>
-          <Header navigation={this.props.navigation}/>
+      <View style={styles.container}>
+        <Header navigation={this.props.navigation} />
+        <View style={styles.profileContainer}>
+          <ProfileData navigation={this.props.navigation} />
+          <View style={styles.postsContainer}>
+            <FlatList
+              data={this.state.usuarios}
+              keyExtractor={item => item.id.toString()}
+              renderItem={({ item }) => (
+                <Image source={{ uri: item.data.photo }} style={styles.postImage} />
+              )}
+              numColumns={3}
+              contentContainerStyle={styles.listContainer}
+            />
+          </View>
         </View>
-        <Text>Perfil</Text>
-        <ProfileData navigation={this.props.navigation} />
-        <FlatList
-          data={this.state.usuarios}
-          keyExtractor={ (item) => item.id.toString()}
-          renderItem={({item}) => <Text>{item.data.owner}</Text>}
-        />
       </View>
-    )
+    );
   }
 }
 
-
-  
-export default Profile
-
 const styles = StyleSheet.create({
-  contenedor: {
-    display: 'flex', 
-    justifyContent: 'center', 
-    alignItems: 'center'
+  container: {
+    flex: 1,
+    backgroundColor: '#FFF',
   },
-  profileData: {
-    margin: 5
-  }
-})
+  profileContainer: {
+    flex: 1,
+    paddingHorizontal: 20,
+    paddingTop: 20,
+    backgroundColor: '#FFF',
+  },
+  postsContainer: {
+    marginTop: 20,
+  },
+  postsText: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 10,
+    color: '#333333',
+  },
+  postImage: {
+    flex: 1,
+    aspectRatio: 1,
+    margin: 1,
+  },
+  listContainer: {
+    flexGrow: 1,
+  },
+});
+
+export default Profile;
